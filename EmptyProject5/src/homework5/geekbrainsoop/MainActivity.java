570c7f4b9eef96637d28bde0132b69826b82a812
ocusnir <102429445+ocusnir@users.noreply.github.com>
2022-06-02 21:56:32 +0300

    Add files via upload

---
+++ EmptyProject5/src/homework5/geekbrainsoop/MainActivity.java
@@ -0,0 +1,8 @@
+package homework5.geekbrainsoop;
+
+public class MainActivity {
+    public static void main(String[] args) {
+        Employee employee = new Employee();
+        employee.displayInfo();
+    }
+}
