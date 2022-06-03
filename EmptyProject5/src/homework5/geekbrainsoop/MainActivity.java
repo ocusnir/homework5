80ee82d07aaaeebdc1b91dd42e171a478e15ef4a
ocusnir <102429445+ocusnir@users.noreply.github.com>
2022-06-03 16:15:41 +0300

    Update MainActivity.java

--- EmptyProject5/src/homework5/geekbrainsoop/MainActivity.java
+++ EmptyProject5/src/homework5/geekbrainsoop/MainActivity.java
@@ -2,7 +2,16 @@ package homework5.geekbrainsoop;
 
 public class MainActivity {
     public static void main(String[] args) {
-        Employee employee = new Employee();
-        employee.displayInfo();
+
+        Employee[] employee = new Employee[5];
+        employee[0] = new Employee("Ivan", "Ivanov", "Ivanov", "Engineer", "ivanov@mailbox.ru", 89231212, 37000, 25);
+        employee[1] = new Employee("Maria", "Livenko", "Sergeevna", "Designer", "maria@mailbox.ru", 89231212, 27000, 43);
+        employee[2] = new Employee("John", "Johnson", "Liverson", "Cleaner", "john@mailbox.ru", 89231212, 13700, 45);
+        employee[3] = new Employee("David", "Martin", "Simon", "CEO of LIFE", "david@mailbox.ru", 89231212, 370000, 40);
+        employee[4] = new Employee("Ryan", "Liam", "Manson", "Director", "ryan@mailbox.ru", 89231212, 30000, 50);
+
+        for (int i=0; i < employee.length; i++) {
+            if (employee[i].age > 40) employee[i].displayInfo();
+        }
     }
 }
